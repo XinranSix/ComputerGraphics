a938282c00a4655fb083270ddd038cc538c91df1
Yao J <jieyao.lxr@foxmail.com>
2024-09-01 13:42:24 +0800

    添加 renderer 的声明

--- core/include/core/base/core.h
+++ core/include/core/base/core.h
@@ -25,3 +25,19 @@
 
 #define CG_CORE_BIND_EVENT_FN(fn)                                                                                      \
     [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+
+namespace CG {
+    template <typename T>
+    using Scope = std::unique_ptr<T>;
+    template <typename T, typename... Args>
+    constexpr Scope<T> CreateScope(Args&&... args) {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
+
+    template <typename T>
+    using Ref = std::shared_ptr<T>;
+    template <typename T, typename... Args>
+    constexpr Ref<T> CreateRef(Args&&... args) {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+} // namespace CG
--- core/include/resources/Image.h
+++ core/include/resources/Image.h
@@ -1,3 +1,40 @@
 #pragma once
 
-class Image {};
+#include <string>
+#include <string_view>
+
+namespace CG {
+
+    class Image {
+
+        enum class Format { None = 0, RGBA, RGBA32F };
+
+    public:
+        Image(std::string_view path);
+
+        Image(uint32_t width, uint32_t height, Format format, const void* data = nullptr);
+
+        ~Image();
+
+        void SetData(const void* data);
+
+        // VkDescriptorSet GetDescriptorSet() const { return m_DescriptorSet; }
+
+        void Resize(uint32_t width, uint32_t height);
+
+        inline uint32_t GetWidth() const { return width_; }
+        inline uint32_t GetHeight() const { return height_; }
+
+    private:
+        // void AllocateMemory(uint64_t size);
+        // void Release();
+
+    private:
+        uint32_t width_ {};
+        uint32_t height_ {};
+
+        std::byte* data_ {};
+
+        std::string filepath_;
+    };
+} // namespace CG
